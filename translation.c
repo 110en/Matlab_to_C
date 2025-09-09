7b5bf268b8ac1a92af5d649987e4aaef0a578a03
110en <alinilf14@gmail.com>
2025-09-09 14:43:26 -0400

    added translation.c and updated it with comment of credintials

---
+++ translation.c
@@ -0,0 +1,10 @@
+/*
+
+    :Created: July 2025
+    :Author: Ali Nilforoushan 
+        - With The Help Of: Ali Ghazizadeh (University of Michigan, Ann Arbor, ECE Department)
+        - Original Matlab Code: Aditya Muppala (University of Michigan, Ann Arbor, ECE Department)
+        - Paper in which this is based on: https://ieeexplore.ieee.org/abstract/document/10549956
+        - Credit Where Credit is Due: FFTW, GSL, Matio
+        
+*/
