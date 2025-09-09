005efec75137b407a71e6b2c8c873e10c407931e
110en <alinilf14@gmail.com>
2025-09-09 14:46:46 -0400

    updated translation.c to have all necessary include statements

--- translation.c
+++ translation.c
@@ -8,3 +8,19 @@
         - Credit Where Credit is Due: FFTW, GSL, Matio
         
 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <complex.h>
+#include <math.h>
+#include <omp.h>
+#include <gsl/gsl_interp.h>
+#include "linspace.h"
+#include "coloncolon.h"
+#include "arr_size.h"
+#include "pole_array.h"
+#include "matio.h"
+#include "fftw3.h"
+#include "fft.h"
